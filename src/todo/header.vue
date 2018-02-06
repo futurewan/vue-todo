b3090738b36ea3906db6bb52bd9daf16421bde24
futurewan <393192789@qq.com>
2018-02-06 21:03:11 +0800

    change doc

--- src/todo/header.vue
+++ src/todo/header.vue
@@ -5,13 +5,13 @@
 </template>
 
 <style lang="less">
-    .header {
-        text-align: center;
-        h1 {
-            color:#FFBD81;
-            margin-top: 50px;
-            font-size: 55px;
-            margin-bottom:30px;
-        }
+.header {
+    text-align: center;
+    h1 {
+        color: #ffbd81;
+        margin-top: 50px;
+        font-size: 55px;
+        margin-bottom: 30px;
     }
+}
 </style>
